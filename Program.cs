50821b47dacb3f227d6abe5e2562c7e3f7398a40
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-01-27 12:11:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Stack<int> pilha = new Stack<int>();
+
+pilha.Push(0);
+pilha.Push(1);
+pilha.Push(2);
+pilha.Push(3);
+pilha.Push(4);
+
+foreach (int itens in pilha)
+{
+    Console.WriteLine(itens);
+}
+Console.WriteLine($"Removendo o elemento: {pilha.Pop()}");
+pilha.Push(5);
+
+foreach (int itens in pilha) {
+    Console.WriteLine(itens); }
